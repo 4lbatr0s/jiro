42f2e38f9fdf448055984d3c9305bd782bee6f4d
4lbatr0s <serhatoner@protonmail.com>
2024-01-22 01:43:31 +0300

    UPDATE middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,9 @@
-import { authMiddleware } from "@kinde-oss/kinde-auth-nextjs/server"
 //INFO: To prevent non-users to go to dashboard, only logged in users can see
 //thats it, import authmiddleware, put the config object, that is all it takes.
+import { authMiddleware } from '@kinde-oss/kinde-auth-nextjs/server'
+
 export const config = {
-    matcher:["/dashboard/:path*", "/auth-callback"]
+  matcher: ['/dashboard/:path*', '/auth-callback'],
 }
 
 export default authMiddleware
