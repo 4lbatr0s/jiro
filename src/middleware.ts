7f904106582c08efe629cac0f43e9cd2f56df2c7
4lbatr0s <serhatoner@protonmail.com>
2024-01-21 22:46:56 +0300

    FIX wrong import for deployment

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import { authMiddleware } from "@kinde-oss/kinde-auth-nextjs/dist/types/server"
+import { authMiddleware } from "@kinde-oss/kinde-auth-nextjs/server"
 //INFO: To prevent non-users to go to dashboard, only logged in users can see
 //thats it, import authmiddleware, put the config object, that is all it takes.
 export const config = {
