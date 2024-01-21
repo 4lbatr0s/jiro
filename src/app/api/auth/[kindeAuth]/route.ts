615b397c16238a00458a33f552ad0826124048f6
4lbatr0s <serhatoner@protonmail.com>
2024-01-21 22:25:39 +0300

    UPDATE package json prisma generate

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,8 +1,12 @@
-import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
-import { NextRequest } from "next/server";
+import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
+import { NextRequest } from 'next/server'
+
+export async function GET(
+  request: NextRequest,
+  { params }: any
+) {
+  const endpoint = params.kindeAuth
+  return handleAuth(request, endpoint)
+}
 
 //INFO: How to implement Kinde Authentication!
-export async function GET(request:NextRequest, {params}:any) {
-	const endpoint = params.kindeAuth;
-	return handleAuth(request, endpoint);
-}
