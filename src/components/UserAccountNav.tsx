5ffd39a1516854884d3f9beb5e65d227b1758dd3
4lbatr0s <serhatoner@protonmail.com>
2024-01-24 00:47:17 +0300

    CREATE stripe webhook secret

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,10 +1,19 @@
 import { getUserSubscriptionPlan } from "@/lib/stripe";
 import React from "react";
-import { DropdownMenu, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 import { Button } from "./ui/button";
 import { Avatar, AvatarFallback } from "./ui/avatar";
 import Image from "next/image";
 import { Icons } from "./Icons";
+import Link from "next/link";
+import { Gem } from "lucide-react";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/server";
 
 interface UserAccountNavProps {
   email: string | undefined;
@@ -36,12 +45,41 @@ const UserAccountNav = async ({
             ) : (
               <AvatarFallback>
                 <span className="sr-only">{name}</span>
-                <Icons.user className="h-4 w-4 text-zinc-900"/>
+                <Icons.user className="h-4 w-4 text-zinc-900" />
               </AvatarFallback>
             )}
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
+      <DropdownMenuContent className="bg-white" align="end">
+        <div className="flex items-center justify-start gap-2 p-2">
+          <div className="flex flex-col space-y-0.5 leading-none">
+            {name && <p className="font-medium text-sm text-black">{name}</p>}
+            {email && (
+              <p className="w-[200px] truncate text-xs text-zinc-700">
+                {email}
+              </p>
+            )}
+          </div>
+        </div>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <Link href="/dashboard">Dashboard</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem asChild>
+          {subscriptionPlan?.isSubscribed ? (
+            <Link href="/dashboard/billing">Manage Subscription</Link>
+          ) : (
+            <Link href="/pricing">
+              Upgrade <Gem className="text-orange-600 h-4 w-4 ml-1.5" />
+            </Link>
+          )}
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+         <DropdownMenuItem className="cursor-pointer">
+            <LogoutLink>Log out</LogoutLink>
+         </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 };
